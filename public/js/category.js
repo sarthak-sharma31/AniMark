ebd7536cda16f0e22fc8dfb8cb5f30cced947ea1
Sarthak sharma <sarthak312005@gmail.com>
2025-03-10 05:59:25 +0530

    added click functionality

--- public/js/category.js
+++ public/js/category.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.a-card').forEach(card => {
-        card.addEventListener('click', () => {
-          const animeId = card.dataset.id;
-          window.location.href = `/anime/${animeId}`;
+    addClick();
+    function addClick(){
+        document.querySelectorAll('.a-card').forEach(card => {
+            card.addEventListener('click', () => {
+                const animeId = card.dataset.id;
+                window.location.href = `/anime/${animeId}`;
+            });
         });
-      });
+    }
     let currentPage = 1;
     let isLoading = false; // Prevent multiple requests at the same time
     const animeCardsContainer = document.querySelector('.anime-cards-container');
@@ -14,18 +17,21 @@ document.addEventListener('DOMContentLoaded', () => {
             if (!isLoading) {
                 isLoading = true; // Mark as loading
                 setTimeout(loadMoreAnimes, 2000); // Add 2-second delay before fetching
+                addClick();
             }
         }
     });
 
     async function loadMoreAnimes() {
         currentPage++;
+        addClick();
         try {
             const response = await fetch(`/category/new?page=${currentPage}`);
             if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
 
             const html = await response.text();
             animeCardsContainer.insertAdjacentHTML('beforeend', html);
+            addClick();
         } catch (error) {
             console.error('Error fetching more anime data:', error);
         } finally {
